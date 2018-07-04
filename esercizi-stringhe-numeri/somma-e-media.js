42ac9f9fff967cecca98ffa33ed2280850f0a2b2
Marco Venturini <ventu87@hotmail.it>
2018-07-04 18:49:08 +0200

    spero di avere risolto anche questo
    appena potete correggetelo grazie!

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -3,3 +3,19 @@
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
 */
+
+var intN1 = 78;
+
+var intN2 = 459;
+
+var intN3 = 49;
+
+var intN4 = 98;
+
+var intN5 = 59;
+
+var addNum = (intN1+intN2+intN3+intN4+intN5);
+
+var averageNum = (addNum)/5;
+
+console.log(`la somma dei 5 numeri è ${addNum} e la media è ${averageNum}`);

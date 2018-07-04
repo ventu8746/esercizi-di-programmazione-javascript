7d819d888a0ca18286d6df90a189e236f21b7f8a
Marco Venturini <ventu87@hotmail.it>
2018-07-04 18:39:42 +0200

    spero di averlo risolto
    perfavore aiutatemi a capire se ho fatto bene l'esercizio!

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -3,3 +3,11 @@
   Scrivi un programma che prenda in input un anno di nascita e determini l'età della persona, calcola anche quanti anni sono necessari per raggiungere i 100. Stampa in output entrambi i risultati.
   http://www.imparareaprogrammare.it
 */
+
+var dateofBirth = 1987;
+
+var intEta = (2018-(dateofBirth));
+
+var yearNecessary = (100-(intEta));
+
+console.log(`l'età della persona in questione è ${intEta} e compierà 100 anni fra ${yearNecessary}`);

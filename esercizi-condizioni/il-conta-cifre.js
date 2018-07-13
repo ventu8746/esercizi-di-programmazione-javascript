84deec6788b42241a2eb4d578227a5377840e921
Marco Venturini <ventu87@hotmail.it>
2018-07-13 12:18:51 +0200

    penso vada bene
    controlalre please

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -3,3 +3,13 @@
   Scrivi un programma che calcoli quante cifre sono contenute in un numero inserito in input (massimo 9999).
   http://www.imparareaprogrammare.it
 */
+var intNum = 1;
+
+if(intNum>=1000){
+  console.log(`ìl numero ha 4 cifre`)}
+  else if(intNum>=100){
+    console.log(`ìl numero ha 3 cifre`)}
+    else if (intNum>=10){
+    console.log(`ìl numero ha 2 cifre`)}
+    else if(intNum>=1){
+      console.log(`ìl numero ha 1 cifra`)};

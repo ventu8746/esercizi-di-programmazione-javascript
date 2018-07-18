9c088e9ddd98a9b852489c243ad61d5d2ac0142d
Marco Venturini <ventu87@hotmail.it>
2018-07-18 19:16:15 +0200

    aggiornamento file
    aggiornamento ma non corregge nessuno quindi bo...

--- esercizi-cicli/rincorri-la-decina.js
+++ esercizi-cicli/rincorri-la-decina.js
@@ -4,3 +4,13 @@
   Hint: per andare a capo stampa '\n'.
   http://www.imparareaprogrammare.it
 */
+var total=0
+
+var i=0;
+while(i<100){
+  total+=i;
+  i++;
+   console.log(`${i}`);
+  if (i===10||i===20||i===30||i===40||i===50||i===60||i===70||i===80||i===90){
+console.log(`\n`)};
+};
